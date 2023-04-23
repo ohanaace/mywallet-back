909ae3023d06c1567b27e57619238ff022613783
suelen <sreginaclsilva@gmail.com>
2023-04-23 19:50:21 -0300

    feature: logout rota /logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import joi from "joi"
 import { v4 as uuid } from "uuid"
 import { db } from "../database.js"
 
-export async function signUp(req, res){
+export async function signUp(req, res) {
     const { name, email, password } = req.body
 
     const signUpSchema = joi.object({
@@ -22,44 +22,46 @@ export async function signUp(req, res){
         const usedEmail = await db.collection("users").findOne({ email })
         if (usedEmail) return res.status(409).send("Email já cadastrado.")
         const passwordHash = bcrypt.hashSync(password, 10)
-       await db.collection("users").insertOne({ name, email, password: passwordHash })
-      res.sendStatus(201)
+        await db.collection("users").insertOne({ name, email, password: passwordHash })
+        res.sendStatus(201)
     } catch (error) {
         res.status(500).send(error.message)
     }
 }
 
 export async function logIn(req, res) {
-    const {email, password} = req.body
+    const { email, password } = req.body
 
     const signInSchema = joi.object({
         password: joi.string().required().min(3),
         email: joi.string().email().required()
     })
-    const newSignIn = { email, password }
-    const validation = signInSchema.validate(newSignIn, { abortEarly: false })
+    const validation = signInSchema.validate(req.body, { abortEarly: false })
     if (validation.error) {
         const error = validation.error.details.map(err => err.message)
         return res.status(422).send(error)
     }
     try {
-        const user = await db.collection("users").findOne({email})
-        if(user && bcrypt.compareSync(password, user.password)){
+        const user = await db.collection("users").findOne({ email })
+        if (user && bcrypt.compareSync(password, user.password)) {
             const token = uuid()
-            await db.collection("sessions").insertOne({userId: user._id, token})
+            await db.collection("sessions").insertOne({ userId: user._id, token })
             res.send(token)
         }
-        if(!user) return res.status(404).send("E-mail não cadastrado.")
-        if(!bcrypt.compareSync(password, user.password)) res.status(401).send("Senha incorreta.")
+        if (!user) return res.status(404).send("E-mail não cadastrado.")
+        if (!bcrypt.compareSync(password, user.password)) res.status(401).send("Senha incorreta.")
     } catch (error) {
         res.status(500).send(error.message)
     }
 }
 
-export async function logOut(){
+export async function logOut(req, res) {
+    const { authorization } = req.headers
+    const token = authorization?.replace("Bearer ", "")
     try {
-        
+        await db.collection("sessions").deleteOne({ token })
+        res.status(200).send("Usuário deslogado")
     } catch (error) {
-        
+        res.status(500).send(error.message)
     }
 }
--- src/routes/auth.routers.js
+++ src/routes/auth.routers.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { signUp, logIn } from "../controllers/auth.controller.js"
+import { signUp, logIn, logOut } from "../controllers/auth.controller.js"
 
 const authRouter = Router()
 
 authRouter.post("/cadastro", signUp)
 authRouter.post("/", logIn)
+authRouter.delete("/logout", logOut)
 
 export default authRouter
