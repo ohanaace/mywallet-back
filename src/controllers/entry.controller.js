5b412bde3b3274d10d79a2231433bbb98adfd27c
suelen <sreginaclsilva@gmail.com>
2023-04-24 13:36:28 -0300

    fix: entry params

--- src/controllers/entry.controller.js
+++ src/controllers/entry.controller.js
@@ -2,21 +2,23 @@ import dayjs from "dayjs"
 import { db } from "../database.js"
 
 export async function newEntry(req, res) {
+    const {tipo} = req.params
     const { value, description } = req.body
     const user = res.locals.user
     const session = res.locals.session
 
     try {
-        if (req.params.tipo === "entrada") {
+        if (tipo === "entrada") {
             await db.collection("registers").insertOne({ value, description, type: "positive", date: dayjs().format("DD/MM"), owner: session.userId })
             res.sendStatus(201)
         }
-        if (req.params.tipo === "saida") {
+        if (tipo === "saida") {
             await db.collection("registers").insertOne({ value, description, type: "negative", date: dayjs().format("DD/MM"), owner: session.userId })
             res.sendStatus(201)
         }
     } catch (error) {
         res.status(500).send(error.message)
+        
     }
 }
 export async function getUserEntries(req, res) {
